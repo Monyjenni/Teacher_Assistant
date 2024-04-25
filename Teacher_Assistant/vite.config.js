6d8b0f96310c3d043f21e6cf08e24c1254f3f82c
Heat Koemnak <heatkimnak@gmail.com>
2024-04-25 14:39:13 +0700

    added bootstrap

--- Teacher_Assistant/vite.config.js
+++ Teacher_Assistant/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                "resources/sass/app.scss",
+                "resources/css/app.css",
+                "resources/js/app.js",
             ],
             refresh: true,
         }),
@@ -22,7 +23,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+            vue: "vue/dist/vue.esm-bundler.js",
         },
     },
 });
